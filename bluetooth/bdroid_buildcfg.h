2c9956ef4a1346c13b17fb03c40dc8faaec3ae23
Wink Saville <wink@google.com>
2012-08-20 14:39:02 -0700

    Add support for per device bluetooth configuration.
    Bug: 6837125
    Change-Id: Ifa631af298c9c78f3317c864ec257915f17775ff

---
+++ bluetooth/bdroid_buildcfg.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+
+#define BTM_DEF_LOCAL_NAME "Nexus 7"
+#define BTIF_HF_SERVICES (BTA_HSP_SERVICE_MASK)
+#define BTIF_HF_SERVICE_NAMES  { BTIF_HSAG_SERVICE_NAME }
+#define BT_CLEAN_TURN_ON_DISABLED  TRUE
+#define HID_HOST_INCLUDED FALSE
+#define PAN_NAP_DISABLED TRUE
+
+#endif
