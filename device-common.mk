0c5027bb07843f4ab03165373a213fc9283ae38a
Ramanan Rajeswaran <ramanan@google.com>
2012-08-27 10:19:31 -0700

    more clean up .
    Change-Id: Idfbd3314cee1f7381e4db2f9877fa838165076b4

--- device-common.mk
+++ device-common.mk
@@ -38,6 +38,7 @@ include frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel \
+    device/asus/grouper/init.grouper.rc:root/init.grouper.rc \
     device/asus/grouper/fstab.grouper:root/fstab.grouper \
     device/asus/grouper/ueventd.grouper.rc:root/ueventd.grouper.rc \
     device/asus/grouper/init.grouper.usb.rc:root/init.grouper.usb.rc \
