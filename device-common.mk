9455d23228b55477bbd59770d297e1beea2badc0
Jeff Sharkey <jsharkey@android.com>
2013-03-13 17:06:45 -0700

    Use dumpstate directly for bugreports.
    Use new broadcast flag on dumpstate to notify framework when a
    bugreport finished.  Remove older shell scripts.
    
    Bug: 7005318
    Change-Id: I6bdf1843cadfb14ced3efdf78bd4591b905bc56f

--- device-common.mk
+++ device-common.mk
@@ -78,12 +78,6 @@ PRODUCT_PACKAGES := \
 PRODUCT_PACKAGES += \
     keystore.grouper
 
-# for bugmailer
-PRODUCT_PACKAGES += send_bug
-PRODUCT_COPY_FILES += \
-    system/extras/bugmailer/bugmailer.sh:system/bin/bugmailer.sh \
-    system/extras/bugmailer/send_bug:system/bin/send_bug
-
 # NFC packages
 PRODUCT_PACKAGES += \
     nfc.grouper \
