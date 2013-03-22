b2ae62071806a476956821a4ce7513c4faf46272
Dmitry Shmidt <dimitrysh@google.com>
2013-03-22 15:30:25 -0700

    grouper: wifi: Remove obsolete path to P2P FW
    Change-Id: I406090bdcc79c45dff7c4c00f94f0cf302a63d4c
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,7 +53,6 @@ BOARD_WLAN_DEVICE           := bcmdhd
 WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
-WIFI_DRIVER_FW_PATH_P2P     := "/vendor/firmware/fw_bcmdhd_p2p.bin"
 
 TARGET_BOOTLOADER_BOARD_NAME := grouper
 TARGET_NO_BOOTLOADER := true
