86ce5f6bb562e179501982c92517618cb549d340
fattire <f4ttire@gmail.com>
2013-07-26 16:31:12 -0700

    remove sepolicy stuff as 10.2 uses same as 10.1 not AOSP
    Change-Id: I0c472b858842836ad984534800930a88ccb80900

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,23 +41,3 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/asus/grouper
 include device/asus/grouper/BoardConfigCommon.mk
 
 TARGET_RECOVERY_FSTAB = device/asus/grouper/fstab.grouper
-
-BOARD_SEPOLICY_DIRS := \
-        device/asus/grouper/sepolicy
-
-BOARD_SEPOLICY_UNION := \
-        file_contexts \
-        genfs_contexts \
-        app.te \
-        btmacreader.te \
-        compatibility.te \
-        device.te \
-        drmserver.te \
-        init_shell.te \
-        file.te \
-        rild.te \
-        sensors_config.te \
-        shell.te \
-        surfaceflinger.te \
-        system.te \
-        zygote.te
