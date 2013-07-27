2c45db0db46870141ee6f96f3ae074169aa13fc8
fattire <f4ttire@gmail.com>
2013-07-27 15:58:52 -0700

    set RECOVERY_FSTAB_VERSION to 2
    Change-Id: Iccad622fec83b8d55aee1eb911ab7e5bd75b7a68

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,6 +34,7 @@ USE_PROPRIETARY_AUDIO_EXTENSIONS := false
 BOARD_LIB_DUMPSTATE := libdumpstate.grouper
 
 TARGET_RECOVERY_UI_LIB := librecovery_ui_grouper
+RECOVERY_FSTAB_VERSION := 2
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/asus/grouper
 
