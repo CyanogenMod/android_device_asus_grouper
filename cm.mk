796d276c6f82624b9e6f38d45dcece46de04cca1
fattire <f4ttire@gmail.com>
2013-11-19 22:11:41 -0800

    update fingerprint to KRT16S
    Change-Id: I05c072ad52696c3d246353baa2cc19f889e31537

--- cm.mk
+++ cm.mk
@@ -22,5 +22,4 @@ PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := Asus
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.4/KRT16M/573038:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.4 KRT16M 573038 release-keys"
-
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.4/KRT16S/573038:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.4 KRT16S 573038 release-keys"
