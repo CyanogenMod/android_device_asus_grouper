d91444faa37cbe8321321a83adf8ce20f49d96e5
dhacker29 <dhackerdvm@gmail.com>
2014-06-06 02:32:12 -0400

    Update fingerprint to 4.4.3
    Change-Id: Ib60668a31111641ff84a1f48855199968302a3bd

--- cm.mk
+++ cm.mk
@@ -17,9 +17,9 @@ $(call inherit-product, device/asus/grouper/full_grouper.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := grouper
 PRODUCT_NAME := cm_grouper
-PRODUCT_BRAND := Google
+PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 7
-PRODUCT_MANUFACTURER := Asus
+PRODUCT_MANUFACTURER := asus
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.4.2/KOT49H/937116:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.4.2 KOT49H 937116 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.4.3/KTU84L/1148727:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.4.3 KTU84L 1148727 release-keys"
