42d4692f5a832037012a4dce00a1d6e9fc7ae65a
linuxx <io.nolinuxnoparty@gmail.com>
2015-01-24 14:03:05 +0100

    Grouper: a few updates
    Change-Id: Ib593c5084e3c572363c0faf26215df83c0bb98de

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -86,7 +86,6 @@ BOARD_RECOVERY_SWIPE := true
 TARGET_KERNEL_SOURCE := kernel/asus/grouper
 TARGET_KERNEL_CONFIG := cyanogenmod_grouper_defconfig
 
-TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
 
 BOARD_SEPOLICY_DIRS += \
         device/asus/grouper/sepolicy
