fcb56a1067ea5cdbd1dd992f823116ecbc4111f2
linuxxxxx <joey@cyanogenmoditalia.it>
2015-03-24 21:31:47 +0100

    Revert "Remove superfluous OpenSSL include paths."
    This was merged from master branch
    
    This reverts commit cbdc84afe81fe8666e5422cfb124b02aa7798806.
    
    Change-Id: I7f31ce5feeed9b5d767974da685e89bb19a0a634
    Signed-off-by: linuxxxxx <joey@cyanogenmoditalia.it>

--- self-extractors/nvidia/staging/keymaster/Android.mk
+++ self-extractors/nvidia/staging/keymaster/Android.mk
@@ -28,6 +28,7 @@ LOCAL_SRC_FILES := \
 
 LOCAL_C_INCLUDES := \
 	libcore/include \
+	external/openssl/include \
 	$(LOCAL_PATH)/../security/tf_sdk/include
 
 LOCAL_CFLAGS := -fvisibility=hidden -Wall -Werror
