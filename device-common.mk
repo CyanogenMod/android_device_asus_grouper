fde048fdb20701f933e90a097080631e1b0e6f5e
Rob <spirit2457@gmail.com>
2015-07-16 13:34:29 +0100

    Grouper: Remove ffmpeg, no longer required and calling this breaks builds
    Change-Id: I2791be994061dda1367b771a8e66b95c40d3a4f8

--- device-common.mk
+++ device-common.mk
@@ -124,9 +124,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    device/asus/grouper/media_codecs.xml:system/etc/media_codecs.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml
-
+    device/asus/grouper/media_codecs.xml:system/etc/media_codecs.xml
 
 # audio mixer paths
 PRODUCT_COPY_FILES += \
